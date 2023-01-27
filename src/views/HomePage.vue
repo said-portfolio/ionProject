545ae5c86ed2aa4b5dfac49593cbd8274eef7b56
syrano1984@gmail.com <syrano1984@gmail.com>
2023-01-27 22:46:43 +0100

    base project

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,65 +1,83 @@
 <template>
   <ion-page>
-    <ion-header :translucent="true">
-      <ion-toolbar>
-        <ion-title>Blank</ion-title>
-      </ion-toolbar>
-    </ion-header>
+    <body>
+      <ion-header :translucent="true">
+        <ion-toolbar>
+          <ion-row class="toolBar">
+            <ion-col size="10">
+              <ion-title>Blank</ion-title>
+            </ion-col>
+            <ion-col size="2"
+            ><ion-grid>
+              <ion-row class="themeBar">
+                <ion-col class="light-mode-icon">
+                  <ion-icon :src="require('@/assets/icons/light.svg')" />
+                </ion-col>
+                <ion-col class="toggle-icon">
+                  <ion-toggle @ionChange="toggle()" :checked="isDark" />
+                </ion-col>
+                <ion-col class="dark-mode-icon">
+                  <ion-icon :src="require('@/assets/icons/dark.svg')" />
+                </ion-col>
+              </ion-row>
+            </ion-grid>
+          </ion-col>
+          </ion-row>
+        </ion-toolbar>
+      </ion-header>
 
-    <ion-content>
-      <ion-list>
-        <ion-item>
-          <ion-checkbox slot="start"></ion-checkbox>
-          <ion-label>
-            <h1>Create Idea</h1>
-            <ion-note>Run Idea By Brandy</ion-note>
-          </ion-label>
-          <ion-badge color="success" slot="end"> 5 Days </ion-badge>
-        </ion-item>
-      </ion-list>
-      <ion-fab vertical="bottom" horizontal="end">
-        <ion-fab-button @click="() => router.push('/new')">
-          <ion-icon :icon="add"></ion-icon>
-        </ion-fab-button>
-      </ion-fab>
-    </ion-content>
+      <ion-content>
+        {{ isDark }}
+      </ion-content>
+    </body>
   </ion-page>
 </template>
 
 <script setup>
-import { add } from 'ionicons/icons';
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/vue';
-import { useRouter } from 'vue-router';
-const router = useRouter()
+import { useDark, useToggle } from "@vueuse/core";
+import {
+  IonPage,
+  IonContent,
+  IonHeader,
+  IonTitle,
+  IonToolbar,
+  IonToggle,
+  IonIcon,
+  IonGrid,
+  IonRow,
+  IonCol,
+} from "@ionic/vue";
+import { watchEffect } from "vue";
 
+const isDark = useDark();
+const toggle = useToggle(isDark);
+
+watchEffect(() => {
+  document.body.setAttribute(
+    "color-mode",
+    isDark.value === true ? "dark" : "auto"
+  );
+});
 </script>
 
-<style scoped>
-#container {
+<style lang="scss" scoped>
+.toolBar {
+  display: flex;
+  justify-content: baseline;
+  align-items: center;
+}
+.themeBar{
+  display: flex;
+  align-items: center;
   text-align: center;
-  
-  position: absolute;
-  left: 0;
-  right: 0;
-  top: 50%;
-  transform: translateY(-50%);
-}
-
-#container strong {
-  font-size: 20px;
-  line-height: 26px;
-}
-
-#container p {
-  font-size: 16px;
-  line-height: 22px;
-  
-  color: #8c8c8c;
-  
-  margin: 0;
-}
-
-#container a {
-  text-decoration: none;
+  .light-mode-icon {
+    text-align: right;
+  }
+  .dark-mode-icon {
+    text-align: left;
+  }
+  .toggle-icon {
+    text-align: center;
+  }
 }
 </style>
